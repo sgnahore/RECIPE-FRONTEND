b4026b550288183621eb84b6ea2c63c7f600baed
Sgnahore <stephaniergnahore@gmail.com>
2023-09-05 15:26:17 +0100

    React Router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./components/App";
 import { ChakraProvider } from "@chakra-ui/react";
+import { DisplaySingleRecipe } from "./components/DisplaySingleRecipe";
+import { Welcome } from "./pages/WelcomePage";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Welcome />,
+    },
+    {
+        path: "recipes",
+        element: <App />,
+    },
+    {
+        path: "recipes/:recipeId",
+        element: <DisplaySingleRecipe />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <ChakraProvider>
         <React.StrictMode>
-            <App />
+            <RouterProvider router={router} />
         </React.StrictMode>
     </ChakraProvider>
 );
